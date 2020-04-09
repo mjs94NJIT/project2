b6976ba52db760016ce79fe33cf0c8736c6fe478
mjs94NJIT <61396958+mjs94NJIT@users.noreply.github.com>
2020-04-09 05:00:12 -0400

    Add files via upload

---
+++ i_node_you_want_me.py
@@ -0,0 +1,157 @@
+import random 
+
+class Node:
+    
+    def __init__(self, v):
+        self.value = v
+        self.adj = {} #adj {adjacent node: weight of the edge}
+#END OF CLASS Node 
+        
+class WeightedGraph:
+    
+    def __init__(self):
+        self.nodes = [] #the list of nodes in the graph
+        
+    def addNode(self, value):
+        self.nodes.append(Node(value))
+    
+    def addDirectedEdge(self, first, second, weight):
+        first.adj[second] = weight
+        
+    def removeUndirectedEdge(self, first, second):
+        if(first.adj[second]):
+            del first.adj[second]
+        
+    def getAllNodes(self):
+        return self.nodes
+
+    #a method for testing that returns a node address that has a given value 
+    def getNodeFromValue(self, value):
+        for node in self.nodes:
+            if node.value is value:
+                return node
+#END OF CLASS DirectedGraph
+
+#helper methods 
+
+def printATraversal(nodeList):
+    if(not nodeList):
+        print('Path not found.')
+    else:
+        for node in nodeList:
+            if not node:
+                continue
+            print(str(node.value)+' ', end="")
+        print()
+
+#start main code 
+
+def createRandomCompleteWeightedGraph(n):
+    graph = WeightedGraph()
+    for iter in range(0,n):
+        graph.addNode(iter)
+    nodeList = graph.getAllNodes()
+    for first in nodeList: 
+        for second in nodeList:
+            if first is not second: #no path to itself
+                first.adj[second] = random.randint(0,99)
+    return graph
+    
+
+def createLinkedList(n): 
+    graph = WeightedGraph()
+    graph.addNode(0)
+    last = graph.getAllNodes()[0]
+    for iter in range(1,n):
+        graph.addNode(iter)
+        newest = graph.getAllNodes()[-1]
+        graph.addDirectedEdge(last, newest, 1)
+        last = newest
+
+def dijkstras(start):
+    distance = {} 
+    previous = {} 
+    queue = []
+    visited = set()
+    for vertex in start.adj:
+        distance[vertex] = float('inf')
+        previous[vertex] = None 
+        if vertex is not start:
+            queue.append(vertex) #tuple of vertex and distance to it 
+            distance[start] = 0 
+        queue.append(start)
+    
+    while queue:
+        shortest = findMin(queue, distance)
+        queue.remove(shortest)
+        visited.add(shortest)
+        for neighbor in shortest.adj: 
+            if neighbor in visited:
+                continue
+            tempDistance = distance[shortest] + shortest.adj[neighbor]
+            print(str(tempDistance) + "= " + str(distance[shortest]) + " + " + str(shortest.adj[neighbor]))
+            if tempDistance < distance[neighbor]:
+                distance[neighbor] = tempDistance
+                previous[neighbor] = shortest
+
+    return distance, previous #returns a tuple
+    
+def findMin(queue, distance):
+    minimumDistance = float('inf')
+    minimumVertex = None
+    for vertex in queue:
+        if distance[vertex] <= minimumDistance:
+            minimumDistance = distance[vertex]
+            minimumVertex = vertex
+    return minimumVertex
+
+
+def testGraph(): 
+    graph = WeightedGraph()
+    for i in range (0, 5):
+        graph.addNode(i)
+    nodes = graph.getAllNodes()
+    graph.addDirectedEdge(nodes[0], nodes[1], 9)
+    graph.addDirectedEdge(nodes[0], nodes[2], 3)
+    graph.addDirectedEdge(nodes[0], nodes[3], 2)
+    graph.addDirectedEdge(nodes[0], nodes[4], 10)
+    graph.addDirectedEdge(nodes[1], nodes[2], 8)
+    graph.addDirectedEdge(nodes[1], nodes[3], 7)
+    graph.addDirectedEdge(nodes[1], nodes[4], 5)
+    graph.addDirectedEdge(nodes[2], nodes[3], 4)
+    graph.addDirectedEdge(nodes[2], nodes[4], 11)
+    graph.addDirectedEdge(nodes[3], nodes[4], 6)
+    return graph
+
+
+
+WGraph = createRandomCompleteWeightedGraph(10)
+LList = createLinkedList(10)
+testgraph = testGraph()
+
+distance, previous = dijkstras(WGraph.getAllNodes()[0])
+
+print("Vertex\tDist.\tParent")
+for vertex in distance: #for vertex key in distance 
+    if(vertex):
+        print(str(vertex.value), end = "\t")
+        print(str(distance[vertex]), end = "\t")
+        if(vertex in previous):
+            print(str(previous[vertex].value))
+        else:
+            print("None")
+            
+distance, previous = dijkstras(testgraph.getAllNodes()[0])
+
+print("Vertex\tDist.\tParent")
+for vertex in distance: #for vertex key in distance 
+    if(vertex):
+        print(str(vertex.value), end = "\t")
+        print(str(distance[vertex]), end = "\t")
+        if(vertex in previous):
+            print(str(previous[vertex].value))
+        else:
+            print("None")
+    
+    
+
---
+++ thank_you_vertext.py
@@ -0,0 +1,148 @@
+import random 
+
+class Node:
+    
+    def __init__(self, v):
+        self.value = v
+        self.adj = set() #adj is the adjacent nodes
+#END OF CLASS Node 
+        
+class DirectedGraph:
+    
+    def __init__(self):
+        self.nodes = [] #the list of nodes in the graph
+        
+    def addNode(self, value):
+        self.nodes.append(Node(value))
+    
+    def addDirectedEdge(self, first, second):
+        first.adj.add(second)
+        
+    def removeUndirectedEdge(self, first, second):
+        first.remove(second)
+        
+    def getAllNodes(self):
+        return self.nodes
+
+    #a method for testing that returns a node address that has a given value 
+    def getNodeFromValue(self, value):
+        for node in self.nodes:
+            if node.value is value:
+                return node
+#END OF CLASS DirectedGraph
+
+class TopSort:
+    
+        
+    def __init__(self):
+        self.visited = set()
+        
+    def Kahns(self, DAGraph):
+        nodeMap = {} #structured {node object: num of incoming edges}
+        retArray = []
+        queue = []
+        for node in DAGraph.getAllNodes(): #build the dictionary 
+            if node not in nodeMap: #the node isn't pointed to by any other nodes
+                nodeMap[node] = 0
+            for adj in node.adj:
+                if adj not in nodeMap:
+                    nodeMap[adj] = 1 #the node has been pointed to for the first time
+                else:
+                    nodeMap[adj] += 1 #else the point is pointed to again 
+        #note: this could be more efficient in the design of node/graph was changed but i'm using the same structure as part 1 
+        
+        for node in nodeMap:
+            if nodeMap[node] is 0: 
+                queue.append(node)
+        
+        while(queue): #while the queue is not empty 
+            node = queue.pop(0) #dequeue
+            retArray.append(node)
+            for adj in node.adj:
+                nodeMap[adj] -= 1
+                if nodeMap[adj] is 0:
+                    queue.append(adj)
+            nodeMap[node] -= 1 
+        
+        return retArray
+
+    def mDFS(self, DAGraph):
+        self.visited.clear()
+        stack = [] 
+        for node in DAGraph.getAllNodes():
+            if node not in self.visited:
+                self.mDFSHelper(node, stack)
+        return stack
+    
+    def mDFSHelper(self, v, stack):
+        self.visited.add(v)
+        for adj in v.adj:
+            if adj not in self.visited:
+                self.mDFSHelper(adj, stack)
+        stack.insert(0, v)
+        return stack
+
+#END OF CLASS TopSort
+
+#helper methods 
+
+def printATraversal(nodeList):
+    if(not nodeList):
+        print('Path not found.')
+    else:
+        for node in nodeList:
+            if not node:
+                continue
+            print(str(node.value)+' ', end="")
+        print()
+
+def testDAGraph():
+    #image included in readme
+    graph = DirectedGraph()
+    for i in range(1, 8):
+        graph.addNode(i)
+    graph.addDirectedEdge(graph.getNodeFromValue(1),graph.getNodeFromValue(2))
+    graph.addDirectedEdge(graph.getNodeFromValue(1),graph.getNodeFromValue(3))
+    graph.addDirectedEdge(graph.getNodeFromValue(2),graph.getNodeFromValue(4))
+    graph.addDirectedEdge(graph.getNodeFromValue(2),graph.getNodeFromValue(5))
+    graph.addDirectedEdge(graph.getNodeFromValue(3),graph.getNodeFromValue(6))
+    graph.addDirectedEdge(graph.getNodeFromValue(4),graph.getNodeFromValue(7))
+    graph.addDirectedEdge(graph.getNodeFromValue(5),graph.getNodeFromValue(7))
+    graph.addDirectedEdge(graph.getNodeFromValue(6),graph.getNodeFromValue(5))
+    graph.addDirectedEdge(graph.getNodeFromValue(6),graph.getNodeFromValue(7))
+    return graph
+    
+#start main code 
+
+def createRandomDAGIter(n):
+    graph = DirectedGraph()
+    for iter in range (0,n):
+        graph.addNode(iter)
+    nodes = graph.getAllNodes()
+    for iter in range(0,n-1):
+        #adds random edges to the graph by only adding them in a forward direction 
+        #i.e. a node will only ever have an edge pointing to a node of a greater value
+        #the last node will be the 'end' of the graph (no edges coming out)
+        graph.addDirectedEdge(nodes[iter], random.choice(nodes[iter+1:]))
+    return graph
+
+sorter = TopSort()
+DAGraph = testDAGraph()
+
+print("On a known graph.")
+print("Kahn's")
+KAHN_Nodes = sorter.Kahns(DAGraph)
+printATraversal(KAHN_Nodes)
+print("mDFS")
+mDFS_Nodes = sorter.mDFS(DAGraph)
+printATraversal(mDFS_Nodes)
+print("On a random graph.")
+
+DAGraph = createRandomDAGIter(1000)
+print("Kahn's")
+KAHN_Nodes = sorter.Kahns(DAGraph)
+printATraversal(KAHN_Nodes)
+print("mDFS")
+mDFS_Nodes = sorter.mDFS(DAGraph)
+printATraversal(mDFS_Nodes)
+
